6f9235903cf935ca3a649a90649f8f69c5a3a6b5
Shariful Islam Mubin <mubin986@gmail.com>
2023-08-15 16:01:53 +0600

    feat: concurrency support

--- src/main.js
+++ src/main.js
@@ -10,58 +10,71 @@ if (!fs.existsSync(mediaDir)) {
   fs.mkdirSync(mediaDir);
 }
 
-const main = async () => {
-  let c = 0;
-  for (const id of vimeoIds) {
-    try {
-      console.log(++c, "/", vimeoIds.length);
-      const video = await getVideoById(id);
-      if (!video) {
-        console.log(`[x] #--> Video ${id} not found`);
-        continue;
-      }
-      const url = getVimeoHighestQualityDownloadLink(video);
-      if (!url) {
-        console.log(`[x] #--> Video ${id} has no download link`);
-        continue;
-      }
-      console.log(id, "->", url);
-      const filename = `${id}.mp4`;
-      const savepath = `${mediaDir}/${filename}`;
-      if (fs.existsSync(savepath)) {
-        console.log(
-          `[###] Video ${id} already downloaded before -> ${savepath}}`
-        );
-        await storage.uploadVideo({
-          filename,
-          filepath: savepath,
-          destination: `backup/${id}-${video.name}.mp4`,
-          showProgress: true,
-          makePublic: true,
-        });
-      } else {
-        await downloadVideoFromUrl({
-          url,
-          savepath,
-          showProgress: true,
-        });
-        await storage.uploadVideo({
-          filename,
-          filepath: savepath,
-          destination: `backup/${id}-${video.name}.mp4`,
-          showProgress: true,
-          makePublic: true,
-        });
-      }
-
-      fs.rmSync(savepath);
+const downloadAndUpload = async ({ id, c, total }) => {
+  try {
+    console.log(c, "/", total);
+    const video = await getVideoById(id);
+    if (!video) {
+      console.log(`[x] #--> Video ${id} not found`);
+      return;
+    }
+    const url = getVimeoHighestQualityDownloadLink(video);
+    if (!url) {
+      console.log(`[x] #--> Video ${id} has no download link`);
+      return;
+    }
+    console.log(id, "->", url);
+    const filename = `${id}.mp4`;
+    const savepath = `${mediaDir}/${filename}`;
+    if (fs.existsSync(savepath)) {
       console.log(
-        "[###] COMPLETE Video",
-        id,
-        "downloaded and uploaded to GCP, removed from local"
+        `[###] Video ${id} already downloaded before -> ${savepath}}`
       );
-    } catch (error) {
-      console.log(c, id, "ERROR", error);
+      await storage.uploadVideo({
+        filename,
+        filepath: savepath,
+        destination: `backup/${id}-${video.name}.mp4`,
+        showProgress: true,
+        makePublic: true,
+      });
+    } else {
+      await downloadVideoFromUrl({
+        url,
+        savepath,
+        showProgress: true,
+      });
+      await storage.uploadVideo({
+        filename,
+        filepath: savepath,
+        destination: `backup/${id}-${video.name}.mp4`,
+        showProgress: true,
+        makePublic: true,
+      });
+    }
+
+    fs.rmSync(savepath);
+    console.log(
+      "[###] COMPLETE Video",
+      id,
+      "downloaded and uploaded to GCP, removed from local"
+    );
+  } catch (error) {
+    console.log(c, id, "ERROR", error);
+  }
+};
+
+const main = async () => {
+  let concurrency = 20;
+
+  let c = 0;
+  let p_arr = [];
+  const total = vimeoIds.length;
+  for (const id of vimeoIds) {
+    c++;
+    p_arr.push(downloadAndUpload({ id, c, total }));
+    if (p_arr.length >= concurrency) {
+      await Promise.all(p_arr);
+      p_arr = [];
     }
   }
 };
